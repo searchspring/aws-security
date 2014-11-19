0f820490f7382173c6dae718d608e8dbe2dee4a7
Eric Herot <eric.github@herot.com>
2014-11-19 13:43:26 -0500

    Clean up whitespace and long lines in group_rule provider

--- providers/group_rule.rb
+++ providers/group_rule.rb
@@ -6,11 +6,11 @@ end
 
 action :add do
   if @current_resource.exists
-  	Chef::Log.info("#{ @new_resource } already exists -- nothing to do")
+    Chef::Log.info("#{ @new_resource } already exists -- nothing to do")
   else
-  	fail "#{ new_reouce } can not be created -- security group does not " \
+    fail "#{ new_reouce } can not be created -- security group does not " \
       'exist' unless security_group
-	  converge_by("Adding rule #{ @new_resource } to security group") do
+    converge_by("Adding rule #{ @new_resource } to security group") do
       from_port = @current_resource.from_port
       to_port = @current_resource.to_port
       security_group.authorize_port_range(from_port..to_port,
@@ -33,10 +33,17 @@ action :remove do
 end
 
 def load_current_resource
-  @current_resource = Chef::Resource::AwsSecurityGroupRule.new(@new_resource.name)
+  @current_resource =
+    Chef::Resource::AwsSecurityGroupRule.new(@new_resource.name)
 
-  @current_resource.aws_access_key_id(@new_resource.aws_access_key_id || node['aws_security']['aws_access_key_id'])
-  @current_resource.aws_secret_access_key(@new_resource.aws_access_key_id || node['aws_security']['aws_secret_access_key'])
+  @current_resource.aws_access_key_id(
+    @new_resource.aws_access_key_id ||
+    node['aws_security']['aws_access_key_id']
+  )
+  @current_resource.aws_secret_access_key(
+    @new_resource.aws_access_key_id ||
+    node['aws_security']['aws_secret_access_key']
+  )
 
   %w(groupname
      name
@@ -50,7 +57,7 @@ def load_current_resource
   end
 
   if @current_resource.port_range
-    (from_port,to_port) = @current_resource.port_range.split(/\.\./)
+    (from_port, to_port) = @current_resource.port_range.split(/\.\./)
     @current_resource.from_port(from_port.to_i)
     @current_resource.to_port(to_port.to_i)
   else
@@ -59,8 +66,8 @@ def load_current_resource
   end
   if new_resource.groupid
     @current_resource.groupid(@new_resource.groupid)
-  elsif sg = security_groupname_exists? 
-    @current_resource.groupid(sg.group_id)
+  elsif security_groupname
+    @current_resource.groupid(security_groupname.group_id)
   # else
     # fail "Could not find security groupid for #{ new_resource }"
   end
@@ -75,15 +82,18 @@ def security_group_rule
   new_ip_permission = current_resource_ip_permissions
   # loop through existing rules looking for our new rule
   security_group.ip_permissions.each do |ip_permission|
-  	# rules are either group based or ip based
-  	group_or_ip = @current_resource.group ? "groups" : "ipRanges"
-  	# if the protocol is '-1' then there aren't from and to ports
-  	return true if @current_resource.ip_protocol == '-1'
-  	# loop through options and make sure they match
+    # rules are either group based or ip based
+    group_or_ip = @current_resource.group ? "groups" : "ipRanges"
+    # if the protocol is '-1' then there aren't from and to ports
+    return true if @current_resource.ip_protocol == '-1'
+    # loop through options and make sure they match
     properties = %w{group ipProtocol fromPort toPort}
-  	current_options = Hash.new
+    current_options = Hash.new
     new_options = Hash.new
-    properties.map{|key| current_options[key] = ip_permission[key] ; new_options[key] = new_ip_permission[key] }
+    properties.map do |key|
+      current_options[key] = ip_permission[key]
+      new_options[key] = new_ip_permission[key]
+    end
     return true if current_options.eql? new_options
   end
   # didn't match anything above, rule doesn't exist
@@ -92,13 +102,18 @@ end
 
 def current_resource_ip_permissions
   rule = {
-  	"groups" 	   => @current_resource.group ? [{ "userId" => @current_resource.owner, "groupId" => current_resource.group }] : [],
-  	"ipRanges" 	 => @current_resource.cidr_ip ? [{ "cidrIp" => @current_resource.cidr_ip } ] : [],
-  	"ipProtocol" => @current_resource.ip_protocol,
+    "groups" 	   => @current_resource.group ?
+      [{ "userId" => @current_resource.owner,
+         "groupId" => current_resource.group }] :
+      [],
+    "ipRanges" 	 => @current_resource.cidr_ip ?
+      [{ "cidrIp" => @current_resource.cidr_ip }] :
+      [],
+    "ipProtocol" => @current_resource.ip_protocol,
   }
   unless rule["ipProtocol"] == '-1'
     rule["fromPort"] = @current_resource.from_port
-  	rule["toPort"] = @current_resource.to_port
+    rule["toPort"] = @current_resource.to_port
   end
   rule
 end
@@ -114,9 +129,12 @@ def construct_security_group_options
 end
 
 def security_group
-	@groupid ||= ec2.security_groups.get_by_id(@current_resource.groupid)
+  @groupid ||= ec2.security_groups.get_by_id(@current_resource.groupid)
 end
 
 def security_groupname
-  @groupname ||= ec2.security_groups.all('group-name' => [@current_resource.groupname]).first
+  @groupname ||=
+    ec2.security_groups.all(
+      'group-name' => [@current_resource.groupname]
+    ).first
 end
