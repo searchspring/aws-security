8e41d3c083eb73941631cb4e79890b993a7950e2
Eric Herot <eric.github@herot.com>
2014-11-20 13:26:13 -0500

    Whitespace and long line cleanup; switch to ruby19 hash syntax

--- providers/group.rb
+++ providers/group.rb
@@ -19,31 +19,36 @@ action :remove do
     fail "#{ @new_resource } cannot be removed - configuration " \
       'mismatch' unless security_group
     converge_by("Remvoing #{ @new_resource } security group") do
-	    security_group.destroy
+      security_group.destroy
     end
   else
-  	Chef::Log.info "#{ @new_resource } does not exist - nothing to do."
+    Chef::Log.info "#{ @new_resource } does not exist - nothing to do."
   end
 end
 
 def load_current_resource
-  @current_resource = Chef::Resource::AwsSecurityGroup.new(@new_resource.groupname)
+  @current_resource =
+    Chef::Resource::AwsSecurityGroup.new(@new_resource.groupname)
 
   %w(groupname
-    description
-    vpcid
-    region).each do |attrib|
+     description
+     vpcid
+     region).each do |attrib|
     @current_resource.send(attrib, @new_resource.send(attrib))
   end
 
-  @current_resource.aws_access_key_id(@new_resource.aws_access_key_id || node['aws_security']['aws_access_key_id'])
-  @current_resource.aws_secret_access_key(@new_resource.aws_secret_access_key || node['aws_security']['aws_secret_access_key'])
+  @current_resource.aws_access_key_id(@new_resource.aws_access_key_id ||
+    node['aws_security']['aws_access_key_id'])
+  @current_resource.aws_secret_access_key(@new_resource.aws_secret_access_key ||
+    node['aws_security']['aws_secret_access_key'])
 
   @current_resource.exists = true if security_group
 end
 
 def security_group
-    @sg ||= ec2.security_groups.all('group-name' => [ @current_resource.groupname ] ).first
+  @sg ||= ec2.security_groups.all(
+            'group-name' => [@current_resource.groupname]
+          ).first
 end
 
 def create_security_group
@@ -52,9 +57,9 @@ end
 
 def attributes
   attributes = {
-    :name         => @current_resource.groupname,
-    :description  => @current_resource.description,
-    :region       => @current_resource.region
+    name:        @current_resource.groupname,
+    description: @current_resource.description,
+    region:      @current_resource.region
   }
   attributes[:vpc_id] = @current_resource.vpcid if @current_resource.vpcid
   attributes
