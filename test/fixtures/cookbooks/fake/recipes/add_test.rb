7227c3b90c225ac92ab1c209322afbb1075459a8
Tim Smith <tsmith@chef.io>
2016-09-06 10:36:48 -0700

    Don't load the data bag twice and use the Chef 12+ helper
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/fixtures/cookbooks/fake/recipes/add_test.rb
+++ test/fixtures/cookbooks/fake/recipes/add_test.rb
@@ -20,12 +20,7 @@
 # limitations under the License.
 #
 
-credentials = Chef::EncryptedDataBagItem.load(
-  node['aws_security']['encrypted_data_bag'],
-  'aws_keys'
-)
-
-credentials = Chef::EncryptedDataBagItem.load(
+credentials = data_bag_item(
   node['aws_security']['encrypted_data_bag'],
   'aws_keys'
 )
