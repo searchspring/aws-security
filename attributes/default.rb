df2608d567de9f00b0c6d0596c31448d57fdc6f9
Tim Smith <tsmith@chef.io>
2016-09-06 10:37:10 -0700

    Don't set an attribute to nil that's only used in testing
    There's no reason to define this on production runs
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,3 @@
-default['aws_security']['encrypted_data_bag'] = nil
 default['aws_security']['aws_access_key_id'] = nil
 default['aws_security']['aws_secret_access_key'] = nil
-default['aws_security']['mocking']               = false
+default['aws_security']['mocking'] = false
